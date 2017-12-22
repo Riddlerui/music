86aeeec42e9f69e8ef95ddccdb83a40da8bff308
xurui <xurui@wzdai.com>
2017-12-22 18:48:18 +0800

    轮播图完成

---
+++ src/test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+ 	hello test!
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  data() {
+    return {};
+  },
+  created() {
+    console.log("---------create");
+  },
+  methods: {}
+};
+</script>
+
+<style lang="less">
+
+</style>
