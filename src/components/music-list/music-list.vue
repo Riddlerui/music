2a77c038348254a897b52bd3f978db5b7de963e5
许蕊 <xurui@xuruideMacBook-Pro.local>
2018-02-28 14:01:27 +0800

    歌曲列表特效

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -5,7 +5,7 @@
     </div>
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
-      <div class="filter" ></div>
+      <div class="filter" ref="filter"></div>
     </div>
     <div class="bg-layer" ref="layer"></div>
     <scroll @scroll="scroll" :probe-type="probeType" :listen-scroll="listenScroll" :data="songs" class="list" ref="list">
@@ -21,6 +21,8 @@
 <script type="text/ecmascript-6">
 import Scroll from 'base/scroll/scroll'
 import SongList from 'base/song-list/song-list'
+
+const RESOVERED_HEIGHT = 40
 
 export default {
   props: {
@@ -47,6 +49,36 @@ export default {
       this.scrollY = pos.y
     }
   },
+  watch: {
+    scrollY(newY){
+      let zIndex = 0
+      let scale = 1
+      let blur = 0
+      let translateY = Math.max(this.minTranslateY, newY);
+      this.$refs.layer.style['transform'] = `translate3d(0,${translateY}px,0)`
+      this.$refs.layer.style['webkitTransform'] = `translate3d(0,${translateY}px,0)`
+      const percent = Math.abs(newY / this.imageHeight);
+      if (newY > 0) {
+        scale = 1 + percent
+        zIndex = 10
+      } else {
+        blur = Math.min(20 * percent, 20)
+      }
+      this.$refs.filter.style['backdrop-filter'] = `blur(${blur})px`
+      this.$refs.filter.style['webkitBackdrop-filter'] = `blur(${blur})px`
+      if (newY < this.minTranslateY) {
+        zIndex = 10
+        this.$refs.bgImage.style.paddingTop = 0
+        this.$refs.bgImage.style.height = `${RESOVERED_HEIGHT}px`
+      } else {
+        this.$refs.bgImage.style.paddingTop = '70%'
+        this.$refs.bgImage.style.height = 0
+      }
+      this.$refs.bgImage.style.zIndex = zIndex
+      this.$refs.bgImage.style['transform'] = `scale(${scale})`
+      this.$refs.bgImage.style['webkitTransform'] = `scale(${scale})`
+    }
+  },
   computed: {
     bgStyle() {
       return `background-image:url(${this.bgImage})`
@@ -57,6 +89,8 @@ export default {
     this.listenScroll = true;
   },
   mounted() {
+    this.imageHeight = this.$refs.bgImage.clientHeight;
+    this.minTranslateY = -this.imageHeight + RESOVERED_HEIGHT;
     this.$refs.list.$el.style.top = `${this.$refs.bgImage.clientHeight}px`
   },
   components: {
