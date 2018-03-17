db11fa954d119721c6174f5f7c52eb9a81c274fd
许蕊 <xurui@xuruideMBP.lan>
2018-03-17 11:13:56 +0800

    歌词滚动列表实现

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -21,6 +21,18 @@
               </div>
             </div>
           </div>
+          <scroll class="middle-r" ref="lyricList" :data="currentLyric && currentLyric.lines">
+            <div class="lyric-wrapper">
+              <div v-if="currentLyric">
+                <p 
+                ref="lyricLine"
+                class="text"
+                :class="{'current': currentLineNum === index}"
+                v-for="(line,index) in currentLyric.lines"
+                :key="index">{{line.txt}}</p>
+              </div>
+            </div>
+          </scroll>
         </div>
         <div class="bottom">
           <div class="progress-wrapper">
@@ -81,6 +93,8 @@ import ProgressBar from "base/progress-bar/progress-bar";
 import ProgressCricle from "base/progress-circle/progress-circle";
 import {playMode} from 'common/js/config';
 import {shuffle} from 'common/js/util';
+import Lyric from 'lyric-parser';
+import Scroll from 'base/scroll/scroll';
 
 const transform = prefixStyle("transform");
 
@@ -89,7 +103,9 @@ export default {
     return {
       songReady: false,
       currentTime: 0,
-      radius: 32
+      radius: 32,
+      currentLyric: null,
+      currentLineNum: 0
     };
   },
   created() {
@@ -121,7 +137,7 @@ export default {
       "playing",
       "currentIndex",
       "mode",
-      "sequenceList"
+      "sequenceList",
     ])
   },
   methods: {
@@ -255,6 +271,24 @@ export default {
       })
       this.setCurrentIndex(index)
     },
+    getLyric() {
+      this.currentSong.getLyric().then((lyric) =>{
+        this.currentLyric = new Lyric(lyric, this.handleLyric)
+        if (this.playing) {
+          this.currentLyric.play()
+        }
+        console.log(this.currentLyric)
+      })
+    },
+    handleLyric({lineNum, txt}) {
+      this.currentLineNum = lineNum
+      if(lineNum > 5) {
+        let lineEl = this.$refs.lyricLine[lineNum - 5]
+        this.$refs.lyricList.scrollToElement(lineEl, 1000)
+      } else {
+        this.$refs.lyricList.scrollTo(0, 0, 1000)
+      }
+    },
     _pad(num, n = 2) {
       let len = num.toString().length;
       while (len < n) {
@@ -293,7 +327,7 @@ export default {
       }
       this.$nextTick(() => {
         this.$refs.audio.play();
-        this.currentSong.getLyric();
+        this.getLyric();
       });
     },
     playing(newPlaying) {
@@ -306,6 +340,7 @@ export default {
   components: {
     ProgressBar,
     ProgressCricle,
+    Scroll,
   }
 };
 </script>
