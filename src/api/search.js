1dae3734d12ac78ad002e3bd2aa1d2024d65dd37
许蕊 <xurui@xuruideMacBook-Pro.local>
2018-03-23 10:34:32 +0800

    搜索框及上拉刷新功能

---
+++ src/api/search.js
@@ -0,0 +1,36 @@
+import jsonp from 'common/js/jsonp'
+import {commonParams, options} from './config'
+
+export function getHotKey () {
+  const url = 'https://c.y.qq.com/splcloud/fcgi-bin/gethotkey.fcg'
+
+  const data = Object.assign({}, commonParams, {
+    needNewCode: 1,
+    platform: 'h5'
+  })
+
+  return jsonp(url, data, options)
+}
+export function search (query, page, zhida, perpage) {
+    const url = 'https://c.y.qq.com/soso/fcgi-bin/search_for_qq_cp'
+  
+    const data = Object.assign({}, commonParams, {
+        w: query,
+        p: page,
+        catzhida: zhida ? 1 : 0,
+        perpage,
+        n:perpage,
+        zhidaqu:1,
+        t:0,
+        ie: 'utf-8',
+        flag:1,
+        sem:1,
+        aggr:0,
+        remoteplace:'txt.mqq.all',
+        needNewCode: 1,
+        uin:0,
+        platform: 'h5'
+    })
+  
+    return jsonp(url, data, options)
+  }
--- src/base/scroll/scroll.vue
+++ src/base/scroll/scroll.vue
@@ -23,6 +23,10 @@ export default {
     listenScroll: {
       type: Boolean,
       default: false
+    },
+    pullup: {
+      type: Boolean,
+      type: false
     }
   },
   mounted() {
@@ -45,6 +49,14 @@ export default {
         this.scroll.on('scroll', (pos) => {
           // 这里的this指的是scroll中的
           me.$emit('scroll', pos)
+        })
+      }
+
+      if(this.pullup){
+        this.scroll.on('scrollEnd', () =>{
+          if(this.scroll.y <= (this.scroll.maxScrollY + 50)){
+            this.$emit('scrollToEnd')
+          }
         })
       }
     },
