ba6d85b9ed6638e7fd214d2365220272018dbff6
许蕊 <xurui@xuruideMacBook-Pro.local>
2018-04-04 15:35:31 +0800

    个人中心完成

--- src/components/user-center/user-center.vue
+++ src/components/user-center/user-center.vue
@@ -1,13 +1,13 @@
 <template>
     <transition name="slider">
         <div class="user-center">
-            <div class="back">
+            <div class="back" @click="back">
                 <i class="icon-back"></i>
             </div>
             <div class="switches-wrapper">
                 <switches :switches="switches" :currentIndex="currentIndex" @switch="switchItem"></switches>
             </div>
-            <div class="play-btn" ref="palyBtn">
+            <div class="play-btn" ref="palyBtn" @click="random">
                 <i class="icon-play"></i>
                 <span class="text">随机播放全部</span>
             </div>
@@ -23,6 +23,9 @@
                     </div>
                 </scroll>
             </div>
+            <div class="no-result-wrapper" v-show="noResult">
+                <no-result :title="noResultDesc"></no-result>
+            </div>
         </div> 
     </transition>
 </template>
@@ -30,11 +33,14 @@
 <script type="text/ecmascript-6">
 import Switches from "base/switches/switches";
 import { mapGetters, mapActions } from "vuex";
-import Scroll from 'base/scroll/scroll';
-import SongList from 'base/song-list/song-list';
-import Song from 'common/js/song';
+import Scroll from "base/scroll/scroll";
+import SongList from "base/song-list/song-list";
+import Song from "common/js/song";
+import NoResult from 'base/no-result/no-result';
+import { playerMixin } from "common/js/mixin";
 
 export default {
+  mixins: [playerMixin],
   data() {
     return {
       currentIndex: 0,
@@ -42,23 +48,55 @@ export default {
     };
   },
   computed: {
+      noResult() {
+          if(this.currentIndex === 0) {
+              return !this.favoriteList.length
+          }else {
+              return !this.playHistory.length 
+          }
+      },
+      noResultDesc() {
+          if(this.currentIndex === 0) {
+              return '暂无收藏歌曲'
+          }else {
+              return '你还没有听过歌曲'
+          }
+      },
     ...mapGetters(["playHistory", "favoriteList"])
   },
   components: {
     Switches,
     Scroll,
-    SongList
+    SongList,
+    NoResult,
   },
   methods: {
+    handlePlaylist(playlist) {
+      const bottom = playlist.length > 0 ? "60px" : "";
+      this.$refs.listWrapper.style.bottom = bottom;
+      this.$refs.favoriteList && this.$refs.favoriteList.refresh();
+      this.$refs.palyList && this.$refs.palyList.refresh();
+    },
     switchItem(index) {
       this.currentIndex = index;
     },
-    selectSong(song){
-        this.insertSong(new Song(song))
+    selectSong(song) {
+      this.insertSong(new Song(song));
     },
-    ...mapActions([
-        'insertSong'
-    ])
+    back() {
+      this.$router.back();
+    },
+    random() {
+      let list = this.currentIndex === 0 ? this.favoriteList : this.playHistory;
+      if(list.length === 0) {
+          return
+      }
+      list = list.map(song => {
+        return new Song(song);
+      });
+      this.randomPlay({ list });
+    },
+    ...mapActions(["insertSong", "randomPlay"])
   }
 };
 </script>
